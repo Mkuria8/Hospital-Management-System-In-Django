e837f16ddba072c5cd50e8fd95f4438703f6b3af
M.Kuria <manasseskush@gmail.com>
2025-03-20 17:43:13 +0300

    prepared  the project for deployment

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mw9n4ohf9-=ve^bvb!^njd*b45v-4ikep3%i^lwjaps^k&o83z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+#SECRET_KEY = env('SECRET_KEY')
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+ 
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'HospitalManagement.urls'
 
@@ -132,3 +138,15 @@ MEDIA_ROOT = BASE_DIR/'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['Hospital-Management-System-In-Django.onrender.com']
+
+import environ
+env = environ.Env()
+DATABASES = {
+    'default': env.db('DATABASE_URL')
+}
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
